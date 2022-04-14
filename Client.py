c88257be227f320c94a953904a6c832ea5c51c6c
Pau J. de Jesus <u1050411@gmail.com>
2022-04-15 01:35:57 +0200

    Solucionat les importacions de crypto y pylab

--- Client.py
+++ Client.py
@@ -6,24 +6,27 @@ import json
 import binascii
 import numpy as np
 import pandas as pd
-#   import pylab as pl
+# import pylab as pl
+import matplotlib
+import matplotlib.pyplot as plt
 import logging
 import datetime
 import collections
 
 #  PKI Requereix les seguents llibreries.
 
-import Crypto
-import Crypto.Random
+import crypto
+import crypto.Random
 import self as self
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
+from crypto.Hash import SHA
+from crypto.PublicKey import RSA
+from crypto.Signature import PKCS1_v1_5
 
 self._private_key = RSA.generate(1024, random)
 self._public_key = self._private_key.publickey()
 
 # Creem la clase publica que sera la identitat del client . En el nostre cas identitat de les universitats.
+
 @property
    def identity(self):
       return
