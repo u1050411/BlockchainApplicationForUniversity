c777470f3c1d749932f1c876e1b301bb07d62984
Pau J. de Jesus <u1050411@gmail.com>
2022-04-15 13:20:40 +0200

    Provant client

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Client import Client
 
 
 def print_hi(name):
