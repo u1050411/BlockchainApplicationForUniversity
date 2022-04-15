7a000e49a81741b3a5f722c45c02c09063439575
Pau J. de Jesus <u1050411@gmail.com>
2022-04-15 13:59:29 +0200

    Clase creació de blocks

---
+++ Transaction.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+# Define una les variables que guardarem en el blockchain
+from matplotlib import collections
+
+
+def __init__(self, sender, recipient, value):
+    self.sender = sender
+    self.recipient = recipient
+    self.value = value
+    self.time = datetime.datetime.now()
+
+# Creem blocks de la cadena
+
+def to_dict(self):
+    if self.sender == "Genesis": # Per tradicció al primer block es anonim i li diem genesis
+        identity = "Genesis"
+    else:
+        identity = self.sender.identity
+
+    return collections.OrderedDict({
+        'sender': identity,
+        'recipient': self.recipient,
+        'value': self.value,
+        'time': self.time})
