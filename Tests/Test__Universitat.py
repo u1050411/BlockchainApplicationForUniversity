d38c7ba79829b4632ad165c41e19eeea98ce1a22
Pau J. de Jesus <u1050411@gmail.com>
2022-04-16 19:32:11 +0200

    Creacio de la Classe Universitat.py i la seva classe test

---
+++ Tests/Test__Universitat.py
@@ -0,0 +1,13 @@
+import unittest
+
+from Universitat import Universitat
+
+
+class TestUniversitat(unittest.TestCase):
+
+    def test_creation(self):
+        udg = Universitat(self)
+        print(udg.nom)
+        print(udg.identity)
+        print(udg.private_key)
+
