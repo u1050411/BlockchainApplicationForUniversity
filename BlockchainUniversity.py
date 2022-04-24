4558667b782dd50fd99b1263ea4019bcdb546552
u1050411@gmail.com <@Nuka2001>
2022-04-24 17:06:09 +0200

    Creacio de la classe factoria

--- BlockchainUniversity.py
+++ BlockchainUniversity.py
@@ -17,22 +17,31 @@ from CreateMysql import MySqlBloc
 
 UTF_8 = 'utf8'
 
+class factoria:
+
+    def __init__(self):
+        self.mydb = MySqlBloc()
+
+    @classmethod
+    def usuari(self, id_usuari):
+        if self.mydb.existeix('BlockchainUniversity', 'usuari', 'id', id_usuari):
+            user = Usuari()
+            sql = f'select * from usuari where id = {id_usuari} LIMIT 1'
+            usuari = (mydb.importar_sql(sql))
+            user.id_usuari = usuari[0]
+            user.nom = usuari[1]
+            user.public_key = mydb.clau_publica(self.id_usuari)
+            retorn = user
+        return retorn
+
 
 class Usuari:
 
-    def __init__(self, id_usuari):
-        mydb = MySqlBloc()
-        mydb.self.mydb.existeix('BlockchainUniversity', 'usuari', 'id', '1070401')
+    def __init__(self):
+        self.id = None
+        self.nom = None
+        self._public_key = None  #
 
-    @classmethod
-    def crear(self, id_usuari):
-        mydb = MySqlBloc()
-        if mydb.existeix('BlockchainUniversity', 'usuari', 'id', id_usuari):
-        sql = f'select * from usuari where id = {id_usuari} LIMIT 1'
-        usuari = (mydb.importar_sql(sql))
-        self.id_usuari = usuari[0]
-        self.nom = usuari[1]
-        self.public_key = mydb.clau_publica(self.id_usuari)
 
     # def sign(self, data):
     #     h = SHA1.new(data)
